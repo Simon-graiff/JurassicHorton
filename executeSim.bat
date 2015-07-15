b9c3d93845e537a30f5b557740a43da3bf28eda3
Christian <klein.chris93@gmail.com>
2015-07-15 11:36:53 -0400

    fixed bat

--- executeSim.bat
+++ executeSim.bat
@@ -1,4 +1,3 @@
 @echo off
-cd C:\Users\Christian\Documents\Kanada\I4Simulation
-start java -Dlog4j.configuration=file:/c:/Users/Christian/Documents/Kanada/I4Simulation/classes/log4j.properties -jar DataAggregator.jar -d 20000 -o "C:\\"
-pause
+cd "I4Simulation"
+start java -Dlog4j.configuration=file:classes/log4j.properties -jar DataAggregator.jar -d 20000 -o "C:\\"
