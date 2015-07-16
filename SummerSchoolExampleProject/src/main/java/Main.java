4f373c45337e2feb80bfb3cd056c8b76c09fba5e
Christian <klein.chris93@gmail.com>
2015-07-16 16:12:50 -0400

    delete batch from project

--- SummerSchoolExampleProject/src/main/java/Main.java
+++ SummerSchoolExampleProject/src/main/java/Main.java
@@ -34,12 +34,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		try {
-			Process p =  Runtime.getRuntime().exec("cmd /c executeSim.bat", null, new File("C:\\Users\\Christian\\Documents\\GitHub\\JurassicHorton"));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		
 		
 		//JAXB Marshalling / Unmarshalling 
 		DemoMarshalling demo = new DemoMarshalling(); 
