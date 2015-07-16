262a104d12f0fd4b399260aa677cd874a4b03011
Julian Gebhart <julian.gebhart@web.de>
2015-07-16 21:18:52 +0200

    Bat time

--- executeSim.bat
+++ executeSim.bat
@@ -1,3 +1,3 @@
 @echo off
 cd "I4Simulation"
-start java -Dlog4j.configuration=file:classes/log4j.properties -jar DataAggregator.jar -d 20000 -o "C:\\"
+start java -Dlog4j.configuration=file:classes/log4j.properties -jar DataAggregator.jar -d 1000 -o "C:\\"
