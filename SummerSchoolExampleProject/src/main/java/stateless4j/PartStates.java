0abf2b39a6349c6b9526b0a6ae6e415dc837e979
Christian <klein.chris93@gmail.com>
2015-07-16 21:19:17 -0400

    states

--- SummerSchoolExampleProject/src/main/java/stateless4j/PartStates.java
+++ SummerSchoolExampleProject/src/main/java/stateless4j/PartStates.java
@@ -3,12 +3,18 @@ package stateless4j;
 public enum PartStates {
 
 	INIT,
-	L1,
-	L2,
-	L3,
-	DRILLING,
-	L4,
-	MILLING,
+	L1_IN,
+	L1_OUT,
+	L2_IN,
+	L2_OUT,
+	L3_IN,
+	MILLING_ON,
+	MILLING_OFF,
+	L3_OUT,
+	L4_IN,
+	DRILLING_ON,
+	DRILLING_OFF,
+	L4_OUT,
 	L5,
 	FINISHED
 	
--- SummerSchoolExampleProject/src/main/java/stateless4j/TestMachine.java
+++ SummerSchoolExampleProject/src/main/java/stateless4j/TestMachine.java
@@ -52,7 +52,7 @@ public class TestMachine {
 	private void configure() {
 		fsmc = new StateMachineConfig<PartStates, Triggers>(); 
 		
-		fsmc.configure(PartStates.INIT)
+	/*	fsmc.configure(PartStates.INIT)
 		.permit(Triggers.DRILLING_ON, PartStates.DRILLING)
 		.permit(Triggers.MILLING_ON, PartStates.MILLING); 
 		
@@ -68,7 +68,7 @@ public class TestMachine {
 		.ignore(Triggers.DRILLING_ON)
 		.ignore(Triggers.DRILLING_OFF)
 		.ignore(Triggers.MILLING_ON)
-		.onEntry(this::increaseMillingCounter);
+		.onEntry(this::increaseMillingCounter);*/
 	}
 
 	/**
